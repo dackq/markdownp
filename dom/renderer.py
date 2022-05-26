fe8b0086ed73e01a4bf43e1c3dc534f71f1a0590
Dakota Reed <dak@archlinux>
2022-05-26 13:01:18 +0200

    Rework DOM.children to contain both str and DOM
    This is a more intuitive format for the DOM object. This way we do not
    have to deal with special "text" DOM objects.

--- dom/renderer.py
+++ dom/renderer.py
@@ -6,33 +6,12 @@ class DOM(object):
     Used to represent a node in an HTML document object model
     """
     element: str
-    children: list['DOM']
+    children: list['DOM' | str]
     text: str
 
-    def __init__(self: 'DOM', element: str, *, children: list['DOM']=[], text: str=""):
+    def __init__(self: 'DOM', element: str, *, children: list['DOM' | str]=[]):
         self.element: str = element
-        self.children: list[DOM] = children
-
-        # The text field is a special case used to contain actual text not html 
-        # For example the text in <p>text</p>. It should be used with the 
-        # element name "text".
-        #
-        # So <p>text</p> would be a DOM object more or less like this: 
-        # (written in json)
-        #   DOM = {
-        #       "element": "p",
-        #       "children": {
-        #           "element": "text",
-        #           "children": [].
-        #           "text": "text"
-        #        }
-        #       "text": ""
-        #   }
-        #
-        # It is implemented this way so that children can be type list['DOM']
-        # rather than list['DOM' | str], as list['DOM' | str] was 
-        # causing problems with the type checker.
-        self.text: str = text
+        self.children: list[DOM | str] = children
 
     def __str__(self):
         return _build_node(self)
@@ -50,10 +29,10 @@ def _build_node(node: DOM) -> str:
 
     html: str = f"<{element}>"
     for next_node in children: 
-        if next_node.element == "text":
-            html += next_node.text
-        else:
-            html += _build_node(next_node)
+        # if next_node is a str then it will be appeneded
+        # if it is a DOM then _build node is called again
+        html = f"{html}{next_node}"
+
     html += f"</{element}>"
 
     return html
