fff73d4e3284fb3afa47a91acf9a6a9ac8e0b79e
Dakota Reed <23349526+dackq@users.noreply.github.com>
2022-06-16 11:26:47 +0200

    Revert "Formatting"
    This reverts commit bc31cb52051964b0bf8060dfd03505986d2fd44c.

--- parsing/tokenizer.py
+++ parsing/tokenizer.py
@@ -6,35 +6,34 @@ import re
 
 block_spec: tuple[tuple[str, str], ...] = (
     # indent (four spaces)
-    ("( {4})", "INDENT"),
-    # whitespace (any amount of spaces less than four (INDENT))
-    ("( *)", "WHITESPACE"),
-    # ordered
-    (" *\\d+\\. .*", "OL_LINE"),
-    # headers
-    ("#{1,6} .+", "ATX_HEADER"),
-)
+    ('( {4})', 'INDENT'),
 
+    # whitespace (any amount of spaces less than four (INDENT))
+    ('( *)', 'WHITESPACE'),
+
+    # ordered
+    (' *\\d+\\. .*', 'OL_LINE'),
+
+    # headers
+    ('#{1,6} .+', 'ATX_HEADER'),
+)
 
 class BlockTokenizer(object):
     """
-    Lazily returns token based on the block structures of a markdown
+    Lazily returns token based on the block structures of a markdown 
     document.
     """
-
     def __init__(self, stream: TextIOBase):
         self._stream: TextIOBase = stream
         self._current_line: str = ""
         self._cursor: int = 0
 
     def _match_line(self, regexp: str, string: str):
-        matched: re.Match[str] | None = re.compile(regexp).match(
-            string, pos=self._cursor
-        )
+        matched: re.Match[str] | None = re.compile(regexp).match(string, pos=self._cursor)
 
         if matched == None:
             return None
-
+        
         # if there was a match then advance the cursor
         self._cursor += len(matched.group())
 
@@ -45,14 +44,13 @@ class BlockTokenizer(object):
         Returns the rest of the line as a string without trying to analyze any
         tokens.
 
-        This is used when a specific structure ignores the token value of a line
+        This is used when a specific structure ignores the token value of a line 
         and just returns a string of plane text. (For example, in a code block)
         """
-        line: str = self._current_line[
-            self._cursor : -1
-        ]  # skip the new line at the end
+        line: str = self._current_line[self._cursor:-1] # skip the new line at the end
         self._cursor = len(self._current_line)
         return line
+
 
     def get_next_token(self) -> dict[str, str]:
         """
@@ -64,19 +62,21 @@ class BlockTokenizer(object):
         if self._cursor >= len(self._current_line) - 1:
             # get the next line from the stream
             self._current_line = self._stream.readline()
-            self._cursor = 0  # reset cursor
+            self._cursor = 0 # reset cursor
 
             # if there are no more lines in the stream return EOF
             if self._current_line == "":
-                return {"type": "EOF"}
+                return {
+                    "type": "EOF"
+                }
 
             # check if the line is blank
-            if re.match("^([ \\t]*)$", self._current_line):
+            if re.match('^([ \\t]*)$', self._current_line):
                 self._cursor = len(self._current_line)
-                return {"type": "BLANK_LINE"}
+                return { "type": "BLANK_LINE" }
 
             # replace all \t with four spaces
-            self._current_line = self._current_line.replace("\t", "    ")
+            self._current_line = self._current_line.replace('\t', '    ')
 
         # try to match with each pattern in block_spec
         for pattern in block_spec:
@@ -85,22 +85,31 @@ class BlockTokenizer(object):
             # if there is a match then return it
             if token != None:
                 # special case tokens
-                if pattern[1] == "WHITESPACE":
+                if pattern[1] == 'WHITESPACE':
                     # skip whitespace
                     continue
-                elif pattern[1] == "INDENT":
-                    return {"type": pattern[1], "value": "\t"}
-                elif pattern[1] == "BLOCK":
-                    return {"type": pattern[1], "value": ""}
+                elif pattern[1] == 'INDENT':
+                    return {
+                        "type": pattern[1], 
+                        "value": "\t"
+                    }
+                elif pattern[1] == 'BLOCK':
+                    return {
+                        "type": pattern[1],
+                        "value": ""
+                        }
 
                 # all other token types are returned here
-                return {"type": pattern[1], "value": token.strip()}
+                return {
+                    "type": pattern[1],
+                    "value": token.strip()
+            }
 
         # if the line does not match any pattern then we just return a normal
         # text line
         text_line = {
-            "type": "TEXT_LINE",
-            "value": self._current_line[self._cursor :].strip(),
+                "type": "TEXT_LINE",
+                "value": self._current_line[self._cursor:].strip()
         }
 
         # and advance the cursor
