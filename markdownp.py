bc31cb52051964b0bf8060dfd03505986d2fd44c
Dakote Reed <dakotalreed@gmail.com>
2022-06-16 11:19:24 +0200

    Formatting

--- markdownp.py
+++ markdownp.py
@@ -8,9 +8,10 @@ from dom.renderer import render, DOM
 from argparse import Namespace
 from sys import argv
 
+
 def main():
     args: Namespace = cli.parse_args(argv[1:])
-    
+
     # setup parsers and renders
     parser: Parser = Parser()
 
@@ -21,6 +22,7 @@ def main():
     # write markdown to new file
     with open(args.output, "w") as output:
         output.write(html)
+
 
 if __name__ == "__main__":
     main()
